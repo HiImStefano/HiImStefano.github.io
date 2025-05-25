25da20855d1a514f1c2b69d7a47d1cc50028ed5d
Stefano <stefanovisentin436@yahoo.com>
2025-05-25 11:42:08 -0400

    Hello Section Completed begining to work about me section

--- src/components/HelloSection.jsx
+++ src/components/HelloSection.jsx
@@ -1,3 +1,5 @@
+import { ArrowDown } from "lucide-react"
+
 export const HelloSection = () => {
     return <section id="hello" className="relative min-h-screen flex flex-col items-center justify-center px-4">
         <div className="container max-w-4xl mx-auto text-center z-10">
@@ -8,10 +10,20 @@ export const HelloSection = () => {
                     <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Visentin</span>
                 </h1>
 
-                <p>
-                   
+                <p className="text-lg md:text-xl text-muted-foregound max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    Create Paragraph about what I do
                 </p>
+
+                <div className="pt-3 opacity-0 animate-fade-in-delay-4">
+                    <a href="#projects" className="galaxy-button">
+                        View my work
+                    </a>
+                </div>
             </div>
+        </div>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+            <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+            <ArrowDown className="h-5 w-5 text-primary" />
         </div>
     </section>
 }
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,6 +2,7 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 import {cn } from "@/lib/utils";
 
+
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
     

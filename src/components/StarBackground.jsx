5195c27277753ae7b4d0fcf146478ab4c16f9562
Stefano <stefanovisentin436@yahoo.com>
2025-05-24 23:31:46 -0400

    Finished up toggle theme button and began working on implementing background on the page

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+
+export const StarBackground = () => {
+    const [star, setStars] = useState([])
+    
+    const generateStars = () => {
+        
+    }
+    return <div> </div>;
+}
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
 import { clsx } from "clsx" 
 import { twMerge } from "tailwind-merge"
 
-export const con = (...inputs) => {
-    return twMerge(clsx())
+export const cn = (...inputs) => {
+    return twMerge(clsx(inputs))
 }
